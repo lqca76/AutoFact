3902b807d0cb2c691925cc06369416f42061d635
Nassim Djoubri <ndjoubri@gmail.com>
2025-08-07 13:56:55 +0200

    feat(configuration): migration des paramètres d'application vers AutoFactConfiguration

--- AutoFactConsole/Program.cs
+++ AutoFactConsole/Program.cs
@@ -1,7 +1,12 @@
-﻿var host = Host.CreateDefaultBuilder(args)
+﻿var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development";
+
+var host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
     {
-        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        config
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environment}.json", optional: true);
     })
     .ConfigureServices((context, services) =>
     {
