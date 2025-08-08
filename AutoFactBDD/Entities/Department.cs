1a82dd78cc733b0ac5c04f996cb7e4b62f6eb9d4
Nassim Djoubri <ndjoubri@gmail.com>
2025-08-08 15:54:20 +0200

    feat(database): ajout du mapping de la table departments et de la colonne departments_id

--- AutoFactBDD/Entities/Department.cs
+++ AutoFactBDD/Entities/Department.cs
@@ -1,12 +1,18 @@
 namespace AutoFactBDD.Entities;
 
+[Table("departments")]
 public class Department
 {
     [Key]
+    [Column("id")]
     public string Id { get; set; } = string.Empty;
 
     [Required]
+    [Column("label")]
     public string Label { get; set; } = string.Empty;
+
+    [Column("docuware_id")]
+    public string DocuWareId { get; set; } = string.Empty;
 
     public virtual ICollection<Invoice> Invoices { get; set; } = [];
 }
