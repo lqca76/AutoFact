ed0a4b81eb83e1074cbaff1d7bafd513bdae9013
Nassim Djoubri <ndjoubri@gmail.com>
2025-08-08 13:15:39 +0200

    docs(core): ajout de commentaires à la méthode Execute de ValidateInvoiceDepartmentUsecase

--- AutoFactCore/UseCases/ValidateInvoiceDepartmentUsecase.cs
+++ AutoFactCore/UseCases/ValidateInvoiceDepartmentUsecase.cs
@@ -13,6 +13,7 @@ public class ValidateInvoiceDepartmentUsecase : IValidateInvoiceDepartmentUsecas
     }
     public async Task<ValidateInvoiceResultDTO> Execute(int invoiceId, bool validate)
     {
+        // Retrieve invoice with its id.
         Invoice? invoice = _invoicesRepository.GetById(invoiceId);
         try
         {
@@ -20,9 +21,15 @@ public class ValidateInvoiceDepartmentUsecase : IValidateInvoiceDepartmentUsecas
                 throw new Exception($"Unable to find invoice with id {invoiceId}");
             if (invoice.PredictedDepartment is null)
                 throw new Exception("No department has been predicted for this invoice.");
+
+            // Update invoice status.
             invoice.Status = InvoiceStatus.Validated;
             _invoicesRepository.Update(invoice);
+
+            // Upload to DocuWare.
             await _docuwareSerivce.Upload(invoice);
+
+            // Return success message.
             return new ValidateInvoiceResultDTO(true, invoice, "Invoice validated and uploaded successfully");
         }
         catch (Exception e)
@@ -30,9 +37,12 @@ public class ValidateInvoiceDepartmentUsecase : IValidateInvoiceDepartmentUsecas
             _logService.Log($"[CORE ERROR] Unable to upload invoice {invoice?.Id ?? invoiceId} : {e}");
             if (invoice is not null)
             {
+                // Update invoice status
                 invoice.Status = InvoiceStatus.Error;
                 _invoicesRepository.Update(invoice);
             }
+
+            // Return error
             return new ValidateInvoiceResultDTO(false, invoice, $"Error uploading invoice: {e.Message}");
         }
     }
